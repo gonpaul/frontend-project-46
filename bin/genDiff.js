d16167720fb9a5a402cf5e16332a630c6593cead
gonpaul <pavelgoncharov56@gmail.com>
2024-05-24 18:22:58 +0300

    step 3

---
+++ bin/genDiff.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+import genDiff from '../index.js';
+
+program
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference')
+    .version('0.0.1')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format [type]', 'output format')
+    .action((filepath1, filepath2) => {
+        genDiff(filepath1, filepath2)
+    })
+
+program.parse();
--- index.js
+++ index.js
@@ -1,5 +1,49 @@
-// @ts-check
+import path from 'node:path';
+import fs from 'fs';
+import _ from 'lodash';
 
-import half from './src/half.js';
+const buildFullPath = (filepath) => path.resolve(process.cwd(), filepath);
 
-export default half;
+const readJsonFile = (filepath) => {
+  try {
+    const absolutePath = buildFullPath(filepath);
+    const data = fs.readFileSync(absolutePath, 'utf-8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Error reading or parsing file: ${filepath}`, error);
+    return null;
+  }
+};
+
+const genDiff = (path1, path2) => {
+  const data1 = readJsonFile(path1);
+  const data2 = readJsonFile(path2);
+
+  if (!data1 || !data2) {
+    return;
+  }
+
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const sortedKeys = _.union(keys1, keys2).sort();
+  console.log(sortedKeys);
+
+  const diff = sortedKeys.reduce((acc, key) => {
+    if (Object.hasOwn(data1, key) && !Object.hasOwn(data2, key)) {
+      acc.push(` - ${key}: ${data1[key]}`);
+    } else if (!Object.hasOwn(data1, key) && Object.hasOwn(data2, key)) {
+      acc.push(` + ${key}: ${data2[key]}`);
+    } else if (data1[key] !== data2[key]) {
+      acc.push(` - ${key}: ${data1[key]}`);
+      acc.push(` + ${key}: ${data2[key]}`);
+    } else {
+      acc.push(`   ${key}: ${data1[key]}`);
+    }
+    return acc;
+  }, []);
+
+  console.log(diff);
+};
+
+export default genDiff;
+
