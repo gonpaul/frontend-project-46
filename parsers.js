34d17c2451b9a9c40c55b0a9e52f43564cad0449
gonpaul <pavelgoncharov56@gmail.com>
2024-05-25 21:01:38 +0300

    extracted general logic into a function in parsers.js

--- parsers.js
+++ parsers.js
@@ -6,12 +6,16 @@ import fs from 'fs';
 import yaml from 'js-yaml';
 
 const buildFullPath = (filepath) => path.resolve(process.cwd(), '__fixtures__', filepath);
+const customReadFile = (filepath) => {
+    const absolutePath = buildFullPath(filepath);
+    // console.log(absolutePath);
+    const data = fs.readFileSync(absolutePath, 'utf-8');
+    return data;
+}
 
 export const parseJsonFile = (filepath) => {
     try {
-        const absolutePath = buildFullPath(filepath);
-        // console.log(absolutePath);
-        const data = fs.readFileSync(absolutePath, 'utf-8');
+        const data = customReadFile(filepath);
         return JSON.parse(data);
     } catch (error) {
         console.error(`Error reading or parsing JSON file: ${filepath}`, error);
@@ -21,9 +25,7 @@ export const parseJsonFile = (filepath) => {
 
 export const parseYamlFile = (filepath) => {
     try {
-        const absolutePath = buildFullPath(filepath);
-        // console.log(absolutePath);
-        const data = fs.readFileSync(absolutePath, 'utf-8');
+        const data = customReadFile(filepath);
         return yaml.load(data);
     } catch (error) {
         console.error(`Error reading or parsing Yaml file: ${filepath}`, error);
